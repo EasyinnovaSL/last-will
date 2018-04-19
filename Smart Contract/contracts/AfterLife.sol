db96c243a100a16922fc23f99a185f213e397a12
Jordi Forroll <jordiforroll@easyinnova.com>
2018-04-19 10:25:36 +0200

    Smart Contract

--- Smart Contract/contracts/AfterLife.sol
+++ Smart Contract/contracts/AfterLife.sol
@@ -23,6 +23,8 @@ contract AfterLife {
 
         //Add Key
         listFileHashmap[msg.sender].listUserKeys[_userWithAccess].push(Key({keyPart: _keyPart, order: _order}));
+
+        require(listFileHashmap[msg.sender].listUsers.length <= 7);
     }
 
     function getMyKeyPart(address _ownerAddress) public view returns (string, uint8){
@@ -37,6 +39,17 @@ contract AfterLife {
 
     function getNumberHierarchyUsers() public view returns (uint) {
         return listFileHashmap[msg.sender].listUsers.length;
+    }
+
+    function getFullKey(address ownerAddress) view  returns (string, string, string, string, string, string, string){
+
+        string[] memory listKeys = new string[](7);
+
+        for (uint j = 0; j < listFileHashmap[ownerAddress].listUserKeys[msg.sender].length; j++) {
+            listKeys[listFileHashmap[ownerAddress].listUserKeys[msg.sender][j].order] =  listFileHashmap[ownerAddress].listUserKeys[msg.sender][j].keyPart;
+        }
+
+        return (listKeys[0], listKeys[1], listKeys[2], listKeys[3], listKeys[4], listKeys[5], listKeys[6]);
     }
 
 }
