bd2d0df502dcc2eb0a7cbdfe874084f8c58300c2
Adrià Llorens <adriallorens@eaysinnova.com>
2018-05-03 10:38:33 +0200

    Change port

--- Smart Contract/truffle-config.js
+++ Smart Contract/truffle-config.js
@@ -8,9 +8,9 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 9595,
+            port: 8545,
             network_id: "*",
-            from: "0xc0Eb469a948C5b7A163Df6e9bCa0a7115a74B7a9",
+            from: "0xff1fbfe19950b12ace5101de1459d765e062cb59",
             //gas: 6712387,
             //gasPrice: 9000000000 // 9 GWei
         },
