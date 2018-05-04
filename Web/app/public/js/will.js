d6335f313fd4740bca71f378c4cfd2a1898f54cc
Xavi <xavitarres@gmail.com>
2018-05-04 10:17:12 +0200

    wills section

--- Web/app/public/js/will.js
+++ Web/app/public/js/will.js
@@ -1,27 +1,53 @@
 function MyWills(options){
     jQuery.extend(options,self.options);
-    this.wills=[];
+    this.data=[];
 this.contract_address=options.contract_address;
+    $('.mbr-more').click($.proxy(this._addWill,this));
+    $('#new-will').submit($.proxy(this._saveWill,this));
 
+    this._listWills();
 }
 
 MyWills.prototype.options={
 
 }
 
-MyWills.prototype._listWills=function(){
-    //this.loading.show();
 
-    var wills = this.getWills();
-    if (wills){
-    this.wills =wills;
+
+MyWills.prototype._addWill=function(){
+$('.wills-form').show();
+}
+MyWills.prototype._saveWill=function(event){
+    event.preventDefault();
+    var formData = $('#new-will').serializeArray();
+    var data={};
+
+    for(var i in formData){
+      data[formData[i].name]=formData[i].value;
     }
-  //  this.lao
+    var heir=[];
+    var type=data['type'];
+    for(var i=0;i<5;i++){
+        if(data['heir'+i]!="")
+            heir.push(data['heir'+i]);
+    }
+
+    this.postWill({'type':type,'heir':heir,'address':Math.random().toString()});
+    $('#new-will')[0].reset();
+    $('.wills-form').hide();
+    $('#OkModal').modal('show');
+    this._listWills();
+}
+MyWills.prototype._listWills=function(){
+    var wills=this.getWills();
+    console.log(JSON.stringify(wills));
+    var template = $('#will-template').html();
+    Mustache.parse(template);   // optional, speeds up future uses
+    var rendered = Mustache.render(template, {'wills':wills});
+    $('.wills-container').html(rendered);
+
 }
 
-MyWills.prototype._addWill={
-
-}
 MyWills.prototype._editWill={
 
 }
@@ -30,10 +56,13 @@ MyWills.prototype._editWill={
 
 }
 MyWills.prototype.getWills= function(){
-    return JSON.parse(localStorage.getItem("wills"));
+    var wills =JSON.parse(localStorage.getItem("wills"));
+    if(wills)
+    this.data = (wills)? wills.wills  :[];
+    return this.data;
 
 }
 MyWills.prototype.postWill= function(will){
-    this.wills[will.adress]=will;
-    localStorage.setItem("wills", JSON.stringify(this.wills))
+    this.data.push(will);
+    localStorage.setItem("wills", JSON.stringify({'wills':this.data}))
 }
