17a0ed38e81184138f4afab9d25fc8443155da18
davidbruguera <davidbruguera@easyinnova.com>
2018-05-15 12:23:52 +0200

    FIX: Check if sum is 100

--- Web/app/public/js/will.js
+++ Web/app/public/js/will.js
@@ -48,15 +48,28 @@ function addRow() {
             break;
     }
 }
+$('.wills-form').show();
 numberOfFields=0;
 function addH(){
-    $("#divholderBen").append("<div id='row"+numberOfFields+"' class='input-group mb-3'><div class='input-group-prepend'><label class='input-group-text' for='inputGroupSelect01'><i class='fa fa-user'></i></label></div><select class='custom-select'  style='height: 49px;' id='inputGroupSelect"+numberOfFields+"' disabled><option value='2' selected>Heir</option></select><div class='input-group-prepend'><label class='input-group-text' for='inputGroupSelect01'>%</label></div><input type='text' id='percent"+numberOfFields+"' class='form-control' style=' padding: 0px;line-height: 23px;font-size: 14px;' aria-label='Amount (to the nearest dollar)'><div class='input-group-append'><div class='input-group-append'><span class='input-group-text'>.00</span></div><button onclick='deleteRow(\"row"+numberOfFields+"\")' class='btn btn-outline-secondary' style='margin: 0px; padding: 0px 10px; font-size: 23px; color: #cc2952;' type='button' >-</button></div></div>");
+    $("#divholderBen").append("<div id='row"+numberOfFields+"' class='input-group mb-3'><div class='input-group-prepend'><label class='input-group-text' for='inputGroupSelect01'><i class='fa fa-user'></i></label></div><select class='custom-select'  style='height: 49px;' id='inputGroupSelect"+numberOfFields+"' disabled><option value='2' selected>Heir</option></select><div class='input-group-prepend'><label class='input-group-text' for='inputGroupSelect01'>%</label></div><input type='text' id='percent"+numberOfFields+"' class='form-control percentatgeRepartir' style=' padding: 0px;line-height: 23px;font-size: 14px;' aria-label='Amount (to the nearest dollar)'><div class='input-group-append'><div class='input-group-append'><span class='input-group-text'>.00</span></div><button onclick='deleteRow(\"row"+numberOfFields+"\")' class='btn btn-outline-secondary' style='margin: 0px; padding: 0px 10px; font-size: 23px; color: #cc2952;' type='button' >-</button></div></div>");
     $("#percent"+numberOfFields).val($("#originalPercent").val());
     numberOfFields++;
 }
 function addW(){
     $("#divholderTes").append("<div id='row"+numberOfFields+"' class='input-group mb-3'><div class='input-group-prepend'><label class='input-group-text' for='inputGroupSelect01'><i class='fa fa-user'></i></label></div><select class='custom-select'  style='height: 49px;' id='inputGroupSelect"+numberOfFields+"' disabled><option value='1' selected>Witness</option></select><div class='input-group-prepend'></div><button onclick='deleteRow(\"row"+numberOfFields+"\")' class='btn btn-outline-secondary' style='margin: 0px; padding: 0px 10px; font-size: 23px; color: #cc2952;' type='button' >-</button></div></div>");
     numberOfFields++;
+}
+function checkIfAllIn(){
+    return true;
+    $i=0;
+    $( ".percentatgeRepartir" ).each(function( index ) {
+        $i=$i+$( this ).val();
+    });
+    if($i!=100){
+        alert('Percentage must sum 100');
+        return false;
+    }
+    return true;
 }
 function deleteRow(rowD){
     $('#'+rowD).remove();
@@ -79,31 +92,33 @@ function selectChanged(){
 }
 
 MyWills.prototype._saveWill = function (event) {
-    event.preventDefault();
-    var formData = $('#new-will').serializeArray();
-    var data = {};
+    if(checkIfAllIn()) {
+        event.preventDefault();
+        var formData = $('#new-will').serializeArray();
+        var data = {};
 
-    for (var i in formData) {
-        data[formData[i].name] = formData[i].value;
-    }
-    var heir = [];
-    var type = data['type'];
-    for (var i = 0; i < 5; i++) {
-        if (data['heir' + i] != "") {
-            heir.push(data['heir' + i]);
+        for (var i in formData) {
+            data[formData[i].name] = formData[i].value;
         }
-    }
+        var heir = [];
+        var type = data['type'];
+        for (var i = 0; i < 5; i++) {
+            if (data['heir' + i] != "") {
+                heir.push(data['heir' + i]);
+            }
+        }
 
-    this.postWill({type: type, heirs: heir}).then(function(txHash){
-        $('#new-will')[0].reset();
-        $('.wills-form').hide();
-        $('#OkModal').modal('show');
-        this._listWills();
-    }.bind(this)).catch(function(err){
-        console.error(err);
-        $('#ErrorModal').find('.modal-body').find('p').html('Something went wrong.');
-        $('#ErrorModal').modal('show');
-    });
+        this.postWill({type: type, heirs: heir}).then(function (txHash) {
+            $('#new-will')[0].reset();
+            $('.wills-form').hide();
+            $('#OkModal').modal('show');
+            this._listWills();
+        }.bind(this)).catch(function (err) {
+            console.error(err);
+            $('#ErrorModal').find('.modal-body').find('p').html('Something went wrong.');
+            $('#ErrorModal').modal('show');
+        });
+    }
 };
 
 MyWills.prototype._declareDead = function (event) {
