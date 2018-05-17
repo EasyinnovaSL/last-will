7a6ea8aa0f9f2ba0dcd2c5204ca1feb129826f92
Adrià Llorens <adriallorens@eaysinnova.com>
2018-05-17 10:58:23 +0200

    Better code

--- Web/app/public/js/will.js
+++ Web/app/public/js/will.js
@@ -3,39 +3,7 @@ function MyWills(options) {
     this.back_to_life = new BackToLife(options);
     this.heritageAbi = options.hierarchy.abi;
     this.lastwill= JSON.parse(localStorage.getItem("lastwill")) || {witness:[],heirs:[],address:''};
-
-
-   // var localWeb3 = new Web3(web3.currentProvider)
-   // var localWeb3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/8tpuK9msQwcod7QVd94H"));
-   // var account=localWeb3.eth.accounts.create([localWeb3.utils.randomHex(32)]);
-
-
-  // console.log(account);
-
-    // account2=localWeb3.eth.accounts.privateKeyToAccount("0x66c14ddb845e629975e138a5c28ad5a72a73552ea65b3d3ec99810c82751cc35");
-    // console.log(  account2);
-    // localWeb3.eth.accounts.wallet.add(account2);
-    //
-    // localWeb3.eth.getAccounts().then( a => console.log(a));
-    // // using the event emitter
-    // localWeb3.eth.sendTransaction({
-    //     from:account2.address,
-    //     to: '0x93fB725d2D8431F7Ab650Ae1892C268C1931f6fA',
-    //     value: web3.toWei(0.01),
-    //     gas:'100000'
-    // })
-    //     .on('transactionHash', function(hash){
-    //     console.log(hash);
-    //     })
-    //     .on('receipt', function(receipt){
-    //    console.log(receipt);
-    //     })
-    //     .on('confirmation', function(confirmationNumber, receipt){
-    //         console.log(confirmationNumber);
-    //         console.log(receipt);
-    //     })
-    //     .on('error', console.error); // If a out of gas error, the second parameter is the receipt.
-
+    this.confirmationsNeeded = 2;
 
     $('#new-will').submit($.proxy(this._saveWill, this));
     $('.wills-container').on('click','button.send',$.proxy(this._sendWill,this));
@@ -73,6 +41,140 @@ MyWills.prototype._withdrawWill = function (event) {
         }.bind(this));
     }
 };
+
+MyWills.prototype._saveWill = function (event) {
+    event.preventDefault();
+    if(checkIfAllIn()) {
+        var localWeb3 = new Web3(web3.currentProvider)
+        var heir=[];
+        var heir_percentage=[];
+        var witness=[];
+        var lastwill = {witness:[],heirs:[],address:''};
+        var self=this;
+
+        $('div.field').each(function (){
+            var account=localWeb3.eth.accounts.create([localWeb3.utils.randomHex(32)]);
+            var type=$(this).find('select').val();
+            if(type == '1' ){
+                lastwill.witness.push({account:account});
+                witness.push(account.address);
+            }else if(type == '2'){
+                lastwill.heirs.push({account:account,percentage:parseInt($(this).find('input').val())});
+                heir.push(account.address);
+                heir_percentage.push($(this).find('input').val());
+            }else if(type == '3'){
+                lastwill.heirs.push({account:account,percentage:parseInt($(this).find('input').val())});
+                lastwill.witness.push({account:account});
+                heir.push(account.address);
+                heir_percentage.push($(this).find('input').val());
+                witness.push(account.address);
+            }
+        });
+
+        if (witness.length < 1) throw new Error("Invalid number of addresses");
+        if (heir.length < 1) throw new Error("Invalid number of addresses");
+        if (heir_percentage && heir_percentage.length !== heir.length) throw new Error("Invalid number of percentages (must be the same as addresses");
+        var totalpercentage=0;
+        heir_percentage.forEach(function (i){
+            totalpercentage+=parseInt(i);
+        })
+        if (totalpercentage !== 100) throw new Error("Invalid percentage (percentage must sum 100");
+        // Input params
+        var addresseswitnesStr = witness.join(";").toLowerCase();
+        var addressesheirsStr = heir.join(";").toLowerCase();
+        var percentagesheirsStr = heir_percentage.join(";").toLowerCase();
+
+        var localWeb3 = new Web3(web3.currentProvider);
+        var contract = new localWeb3.eth.Contract(contracts.base.abi, contracts.base.address);
+
+        var inputValue = $("#ethToContract").val();
+        if (!inputValue) inputValue = 0;
+        contract.methods.createHierarchyContractPayable(addressesheirsStr, percentagesheirsStr,addresseswitnesStr).send({from: web3.eth.defaultAccount,gas: 4500000,value: web3.toWei(inputValue,"ether")})
+            .on('transactionHash', function(hash){
+                $('#OkModal').modal('show');
+                $('#OkModal p').html("Confirming transaction...");
+                $('#OkModal .btn-success').hide();
+                $('#OkModal .close').hide();
+            })
+            .on('confirmation', function(confirmationNumber, receipt){
+                if (confirmationNumber === this.confirmationsNeeded) {
+                    contract.methods.getMyContracts().call({from: web3.eth.defaultAccount},function(err, addressesStr){
+                        // Generate Links
+                        console.log("Addresses: " + addressesStr);
+                        if (addressesStr.slice(-1) === ";") addressesStr = addressesStr.substring(0, addressesStr.length -2);
+                        var adrList = addressesStr.split(";");
+                        var address=adrList[adrList.length-1];
+                        console.log("Address: " + address);
+                        lastwill.heirs.forEach(function(item){
+                            item.url=this._createLink("2",item.account,address)
+                        }.bind(this));
+
+                        lastwill.witness.forEach(function(item){
+                            item.url=this._createLink("1",item.account,address)
+                        }.bind(this));
+
+                        this.lastwill=lastwill;
+                        localStorage.setItem("lastwill",JSON.stringify(this.lastwill));
+
+                        // Final render
+                        $('#OkModal p').html("Transaction send successfully!");
+                        $('#OkModal .btn-success').show();
+                        $('#OkModal .close').show();
+                        this.renderLastWill(this.lastwill);
+                        this._listWills();
+
+                    }.bind(this))
+                }
+            }.bind(this))
+            .on('error', console.error); // If a out of gas error, the second parameter is the receipt.
+    }
+};
+
+MyWills.prototype._listWills = function () {
+    $('.wills-container').html($("#generic-loader").clone());
+    this.getWills().then(function(wills){
+        if (wills !== null && wills && wills.length !== 0) {
+            var template = $('#will-template').html();
+            Mustache.parse(template);   // optional, speeds up future uses
+            var rendered = Mustache.render(template, {'wills': wills});
+            $('.wills-container').html(rendered);
+        } else {
+            $('.wills-container').html("");
+        }
+    }).catch(function(err){
+        console.error(err);
+        $('#ErrorModal').find('.modal-body').find('p').html('Something went wrong listing wills.');
+        $('#ErrorModal').modal('show');
+    });
+
+};
+
+MyWills.prototype._createLink = function(type,account,will){
+    if(type=="1"){
+        return window.location.protocol + '//' + window.location.host+'/witness?pk='+account.privateKey+'&will='+will;
+    }else{
+        return window.location.protocol + '//' + window.location.host+'/heir?pk='+account.privateKey+'&will='+will;
+    }
+}
+
+MyWills.prototype.getWills = function () {
+    console.log("My Ether address: " + web3.eth.defaultAccount);
+    return this.back_to_life.getWills();
+};
+
+MyWills.prototype.postWill = function (addresseswitnes, addressesheirs, percentagesheirs ) {
+    return this.back_to_life.createVoteWill(addresseswitnes, addressesheirs, percentagesheirs);
+};
+
+MyWills.prototype.renderLastWill = function (lastWill) {
+    var template = $('#last-will-added-template').html();
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, lastWill);
+    $('#last-will-links').html(rendered);
+    $('[data-toggle="tooltip"]').tooltip();
+};
+
+
 
 numberOfFields = 0;
 percentagesModified = false;
@@ -151,179 +253,3 @@ function selectChanged(rowNumber){
             break;
     }
 }
-
-
-MyWills.prototype._saveWill = function (event) {
-    event.preventDefault();
-    if(checkIfAllIn()) {
-        var localWeb3 = new Web3(web3.currentProvider)
-        var heir=[];
-        var heir_percentage=[];
-        var witness=[];
-        var lastwill = {witness:[],heirs:[],address:''};
-        var self=this;
-
-        $('div.field').each(function (){
-            var account=localWeb3.eth.accounts.create([localWeb3.utils.randomHex(32)]);
-            var type=$(this).find('select').val();
-            if(type == '1' ){
-                lastwill.witness.push({account:account});
-                witness.push(account.address);
-            }else if(type == '2'){
-                lastwill.heirs.push({account:account,percentage:parseInt($(this).find('input').val())});
-                heir.push(account.address);
-                heir_percentage.push($(this).find('input').val());
-            }else if(type == '3'){
-                lastwill.heirs.push({account:account,percentage:parseInt($(this).find('input').val())});
-                lastwill.witness.push({account:account});
-                heir.push(account.address);
-                heir_percentage.push($(this).find('input').val());
-                witness.push(account.address);
-            }
-        });
-
-        if (witness.length < 1) throw new Error("Invalid number of addresses");
-        if (heir.length < 1) throw new Error("Invalid number of addresses");
-        if (heir_percentage && heir_percentage.length !== heir.length) throw new Error("Invalid number of percentages (must be the same as addresses");
-        var totalpercentage=0;
-        heir_percentage.forEach(function (i){
-            totalpercentage+=parseInt(i);
-        })
-        if (totalpercentage !== 100) throw new Error("Invalid percentage (percentage must sum 100");
-        // Input params
-        var addresseswitnesStr = witness.join(";").toLowerCase();
-        var addressesheirsStr = heir.join(";").toLowerCase();
-        var percentagesheirsStr = heir_percentage.join(";").toLowerCase();
-
-        var localWeb3 = new Web3(web3.currentProvider);
-        var contract = new localWeb3.eth.Contract(contracts.base.abi, contracts.base.address);
-
-        var inputValue = $("#ethToContract").val();
-        if (!inputValue) inputValue = 0;
-        contract.methods.createHierarchyContractPayable(addressesheirsStr, percentagesheirsStr,addresseswitnesStr).send({from: web3.eth.defaultAccount,gas: 4500000,value: web3.toWei(inputValue,"ether")})
-            .on('transactionHash', function(hash){
-                $('#OkModal').modal('show');
-                $('#OkModal p').html("Confirming transaction...");
-                $('#OkModal .btn-success').hide();
-                $('#OkModal .close').hide();
-            })
-            .on('confirmation', function(confirmationNumber, receipt){
-                if (confirmationNumber === 6) {
-                    contract.methods.getMyContracts().call({from: web3.eth.defaultAccount},function(err, addressesStr){
-                        // Generate Links
-                        console.log("Addresses: " + addressesStr);
-                        if (addressesStr.slice(-1) === ";") addressesStr = addressesStr.substring(0, addressesStr.length -2);
-                        var adrList = addressesStr.split(";");
-                        var address=adrList[adrList.length-1];
-                        console.log("Address: " + address);
-                        lastwill.heirs.forEach(function(item){
-                            item.url=this._createLink("2",item.account,address)
-                        }.bind(this));
-
-                        lastwill.witness.forEach(function(item){
-                            item.url=this._createLink("1",item.account,address)
-                        }.bind(this));
-
-                        this.lastwill=lastwill;
-                        localStorage.setItem("lastwill",JSON.stringify(this.lastwill));
-
-                        // Final render
-                        $('#OkModal p').html("Transaction send successfully!");
-                        $('#OkModal .btn-success').show();
-                        $('#OkModal .close').show();
-                        this.renderLastWill(this.lastwill);
-                        this._listWills();
-
-                    }.bind(this))
-                }
-            }.bind(this))
-            .on('error', console.error); // If a out of gas error, the second parameter is the receipt.
-            /*
-            .then(function (receip) {
-                console.log(receip);
-                $('#new-will')[0].reset();
-                $('.wills-form').hide();
-                $('#divholderBen').html("");
-                $('#divholderTes').html("");
-                $('#OkModal').modal('show');
-                var template = $('#last-will-added-template').html();
-                Mustache.parse(template);   // optional, speeds up future uses
-                this.lastwill=lastwill;
-                var rendered = Mustache.render(template,  this.lastwill);
-                $('.lastWillAdded').html(rendered);
-                this._listWills();
-            }.bind(this));*/
-
-        /*return new Promise(function(resolve, reject){
-            // console.log("Params:");
-            // console.log("  " + addressesStr);
-            // console.log("  " + percentagesStr);
-            contract.createHierarchyContractPayable.sendTransaction(addressesheirsStr, percentagesheirsStr,addresseswitnesStr,{gas: 4500000,value: web3.toWei(1,"ether")}, function(err, txHash){
-                if (err) return reject(err);
-                resolve(txHash);
-            });
-        });*/
-
-       /* this.postWill(witness, heir, heir_percentage).then(function (txHash) {
-            $('#new-will')[0].reset();
-            $('.wills-form').hide();
-            $('#divholderBen').html("");
-            $('#divholderTes').html("");
-            $('#OkModal').modal('show');
-            var template = $('#last-will-added-template').html();
-            Mustache.parse(template);   // optional, speeds up future uses
-            this.lastwill=lastwill;
-            var rendered = Mustache.render(template,  this.lastwill);
-            $('.lastWillAdded').html(rendered);
-            this._listWills();
-        }.bind(this)).catch(function (err) {
-            console.error(err);
-            $('#ErrorModal').find('.modal-body').find('p').html('Something went wrong.');
-            $('#ErrorModal').modal('show');
-        });*/
-    }
-};
-
-MyWills.prototype._listWills = function () {
-    $('.wills-container').html($("#generic-loader").clone());
-    this.getWills().then(function(wills){
-        if (wills !== null && wills && wills.length !== 0) {
-            var template = $('#will-template').html();
-            Mustache.parse(template);   // optional, speeds up future uses
-            var rendered = Mustache.render(template, {'wills': wills});
-            $('.wills-container').html(rendered);
-        } else {
-            $('.wills-container').html("");
-        }
-    }).catch(function(err){
-        console.error(err);
-        $('#ErrorModal').find('.modal-body').find('p').html('Something went wrong listing wills.');
-        $('#ErrorModal').modal('show');
-    });
-
-};
-
-MyWills.prototype._createLink = function(type,account,will){
-    if(type=="1"){
-        return window.location.protocol + '//' + window.location.host+'/witness?pk='+account.privateKey+'&will='+will;
-    }else{
-        return window.location.protocol + '//' + window.location.host+'/heir?pk='+account.privateKey+'&will='+will;
-    }
-}
-
-MyWills.prototype.getWills = function () {
-    console.log("My Ether address: " + web3.eth.defaultAccount);
-    return this.back_to_life.getWills();
-};
-
-MyWills.prototype.postWill = function (addresseswitnes, addressesheirs, percentagesheirs ) {
-    return this.back_to_life.createVoteWill(addresseswitnes, addressesheirs, percentagesheirs);
-};
-
-MyWills.prototype.renderLastWill = function (lastWill) {
-    var template = $('#last-will-added-template').html();
-    Mustache.parse(template);
-    var rendered = Mustache.render(template, lastWill);
-    $('#last-will-links').html(rendered);
-    $('[data-toggle="tooltip"]').tooltip();
-};
